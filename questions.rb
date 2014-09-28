7dd94864e691a9f1beaa85e398e2718b6f40ef5c
byverdu <byverdu@gmail.com>
2014-09-28 02:28:50 +0100

    tests 9 to 12, jumping 13

--- questions.rb
+++ questions.rb
@@ -16,6 +16,7 @@ def remove_nils_and_false_from_array(array)
 	array.reject { |word| word === nil || word === false}
 end
 
+##
 def reverse_every_element_in_array(array)
 	array.map { |item| item.reverse  }
 end
@@ -34,3 +35,59 @@ end
 def add_element_to_beginning_of_array(array,new_item)
 	array.insert(0,new_item)
 end
+##
+
+def array_sort_by_last_letter_of_word(array)
+	arr = []
+
+	array.map { |item| arr << item.reverse }
+
+	arr.sort.map { |item| item.reverse  }
+end
+
+def get_first_half_of_string(string)
+	limit_word = (string.length/2.0).ceil
+	string.slice(0,limit_word)
+end
+
+def make_numbers_negative(number)
+	-number.abs
+end
+
+def separate_array_into_even_and_odd_numbers(array)
+	arr1, arr2, arr3 = [],[],[]
+	
+	array.each do |e| 
+
+		arr2 << e if e % 2 == 0 
+		arr1 << e if e % 2 != 0 
+	end
+	
+	arr3<<arr2<<arr1
+end
+
+##
+
+def shortest_word_in_array(array)
+	
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
